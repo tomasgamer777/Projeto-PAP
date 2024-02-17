9a816dda55bdc36c6a4ade50c833b57de134c0bc
tomasgamer777 <tomascalcada10@gmail.com>
2024-02-17 21:05:22 +0000

    Update inscricao.php

--- inscricao.php
+++ inscricao.php
@@ -30,7 +30,7 @@ move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file);
 
 
 $sql = "INSERT INTO inscricao (Nome, Sobrenome, Data_Nasc, Morada, Distrito, Cod_Postal, Email, Foto_inscri, Password)
-VALUES ('$nome', '$sobrenome', '$data_nascimento', '$rua', '$distrito', '$codigo_postal', '$email', '$senha')";
+VALUES ('$nome', '$sobrenome', '$data_nascimento', '$rua', '$distrito', '$codigo_postal', '$email', '$target_file', '$senha')";
 
 if ($conn->query($sql) === TRUE) {
   echo "Inscrição feita com sucesso!";
