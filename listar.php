fc3e9035fc379c622a8529b1d7b1e2925ab1b62f
tomasgamer777 <tomascalcada10@gmail.com>
2024-04-09 22:47:56 +0100

    Update listar.php

--- listar.php
+++ listar.php
@@ -1,26 +1,38 @@
 <?php
-    echo '<h3> Listar Utilizadores </h3>';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user_id = $_POST['user_id'];
+    echo 'Utilizador a procurar:'.$user_id.'<br />';
+    
     $servername = "plesk2.server.highcloudservices.eu";
     $username = "tomas";
-    $password = "Pv~i23i20";
+    $password = "!6B0ire55";
     $dbname = "banda";
 
+    // Cria uma conexão com o banco de dados
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // Verifica a conexão
     if ($conn->connect_error) {
-     die("conexão falada: " . $conn->connect_error);
+        die("conexão falada: " . $conn->connect_error);
     }
-    else {
-        $resultado = $mysqli -> query("select * from users");
-        $n_registos = $resultado -> num_rows;
-        echo $n_registos .' utilizador(es) encontrado(s) <p>';
-        while ($registo = $resultado -> fetch_assoc()) {
-            echo $registo ['user_id'] . " - " .
-                 $registo ['nome'] . " - " .
-                 $registo ['sobrenome'] . " - " ; echo "</br>";
+    
+    // Consulta SQL para selecionar o utilizador com base no user_id fornecido
+    $sql = "SELECT user_id, nome, subrenome FROM users WHERE user_id = '$user_id'";
+    $result = $conn->query($sql);
+
+    // Verifica se há resultados e exibe na página
+    if ($result->num_rows > 0) {
+        echo "<h3>Dados deste utilizador</h3>";
+        while ($row = $result->fetch_assoc()) {
+            echo $row['user_id'] . " - " . $row['nome'] . " - " . $row['subrenome'] . "<br>";
         }
-        $mysqli->close();
+    } else {
+        echo "<h3>Utilizador não encontrado</h3>";
     }
+    
+    // Fecha a conexão com o banco de dados
+    $conn->close();
+}
 ?>
-<p> </p>
-<a href="admin.html"> Voltar a entrada </a>
+<p></p>
+<a href="index.html">Voltar à entrada</a>
