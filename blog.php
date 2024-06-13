780d8a4e8cade8e54e5c8d2275d50c136f0b9919
Calcadinha <89658068+tomasgamer777@users.noreply.github.com>
2024-06-13 22:48:44 +0100

    a

--- blog.php
+++ blog.php
@@ -51,78 +51,38 @@
 							<div class="col-md-7">
 								<div class="content">
 									<h2 class="entry-title">Blog</h2>
-
+			
+									<?php
+									// Assuming $posts is an array containing data fetched from the database
+									foreach ($posts as $post) {
+										$dia = $post['dia'];
+										$mes = $post['mes'];
+										$titulo = $post['titulo'];
+										$descricao = $post['descricao'];
+										$foto = $post['foto'];
+									?>
+			
 									<div class="post">
 										<div class="entry-date">
-											<div class="date">23</div>
-											<span class="month">Jun</span>
+											<div class="date"><?php echo $dia; ?></div>
+											<span class="month"><?php echo $mes; ?></span>
 										</div>
 										<div class="featured-image">
-											<img src="dummy/featured-image-1.jpg" alt="">
+											<img src="<?php echo $foto; ?>" alt="">
 										</div>
-										<h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati officia quo dicta, similique, reiciendis beatae enim adipisci voluptates molestiae iusto, totam harum aliquid. Non officiis nobis, ut nesciunt eligendi asperiores.</p>
+										<h2 class="entry-title"><a href="#"><?php echo $titulo; ?></a></h2>
+										<p><?php echo $descricao; ?></p>
 										<a href="#">Read more</a>
 									</div>
-
-									<div class="post">
-										<div class="entry-date">
-											<div class="date">23</div>
-											<span class="month">Jun</span>
-										</div>
-										<div class="featured-image">
-											<img src="dummy/featured-image-2.jpg" alt="">
-										</div>
-										<h2 class="entry-title"><a href="#">Consectetur adipisicing elit</a></h2>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati officia quo dicta, similique, reiciendis beatae enim adipisci voluptates molestiae iusto, totam harum aliquid. Non officiis nobis, ut nesciunt eligendi asperiores.</p>
-										<a href="#">Read more</a>
-									</div>
-
-									<div class="post">
-										<div class="entry-date">
-											<div class="date">23</div>
-											<span class="month">Jun</span>
-										</div>
-										<div class="featured-image">
-											<img src="dummy/featured-image-3.jpg" alt="">
-										</div>
-										<h2 class="entry-title"><a href="#">Adipisci voluptates molestiae iusto</a></h2>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati officia quo dicta, similique, reiciendis beatae enim adipisci voluptates molestiae iusto, totam harum aliquid. Non officiis nobis, ut nesciunt eligendi asperiores.</p>
-										<a href="#">Read more</a>
-									</div>
-
-									<div class="post">
-										<div class="entry-date">
-											<div class="date">23</div>
-											<span class="month">Jun</span>
-										</div>
-										<div class="featured-image">
-											<img src="dummy/featured-image-4.jpg" alt="">
-										</div>
-										<h2 class="entry-title"><a href="#">Obcaecati officia quo dicta</a></h2>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati officia quo dicta, similique, reiciendis beatae enim adipisci voluptates molestiae iusto, totam harum aliquid. Non officiis nobis, ut nesciunt eligendi asperiores.</p>
-										<a href="#">Read more</a>
-									</div>
-
-									<div class="post">
-										<div class="entry-date">
-											<div class="date">23</div>
-											<span class="month">Jun</span>
-										</div>
-										<div class="featured-image">
-											<img src="dummy/featured-image-5.jpg" alt="">
-										</div>
-										<h2 class="entry-title"><a href="#">Nesciunt eligendi asperiores</a></h2>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati officia quo dicta, similique, reiciendis beatae enim adipisci voluptates molestiae iusto, totam harum aliquid. Non officiis nobis, ut nesciunt eligendi asperiores.</p>
-										<a href="#">Read more</a>
-									</div>
+			
+									<?php } // End foreach ?>
+			
 								</div>
 							</div>
 						</div>
 					</div>
 				</div> <!-- .testimonial-section -->
-
-				
+			
 			</main> <!-- .main-content -->
 
 			<footer class="site-footer">
