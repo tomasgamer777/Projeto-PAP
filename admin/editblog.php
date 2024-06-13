cbeffed343fb7cdb7c8ab762e3cb9ce1f653d6f7
Calcadinha <89658068+tomasgamer777@users.noreply.github.com>
2024-06-13 21:19:07 +0100

    Update editblog.php

--- admin/editblog.php
+++ admin/editblog.php
@@ -378,101 +378,39 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header card-header-primary card-header-icon">
-                        <div class="card-icon">
-                            <i class="material-icons">assignment</i>
-                        </div>
-                        <h4 class="card-title">Lista das informações do Blog</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="toolbar"></div>
-                        <div class="material-datatables">
-                            <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
-                                <thead>
-                                    <tr>
-                                        <th>Código</th>
-                                        <th>Foto</th>
-                                        <th>Dia</th>
-                                        <th>Mês</th>
-                                        <th>Título</th>
-                                        <th>Legenda</th>
-                                        <th class="disabled-sorting text-right">Ações</th>
-                                    </tr>
-                                </thead>
-                                <tfoot>
-                                    <tr>
-                                        <th>Código</th>
-                                        <th>Foto</th>
-                                        <th>Dia</th>
-                                        <th>Mês</th>
-                                        <th>Título</th>
-                                        <th>Legenda</th>
-                                        <th class="text-right">Ações</th>
-                                    </tr>
-                                </tfoot>
-                                <tbody>
-                                <?php
-                                // Conexão com o banco de dados
-                                $servername = "localhost";
-                                $username = "tomas";
-                                $password = "!h01fFw35";
-                                $dbname = "banda";
-
-                                $conn = new mysqli($servername, $username, $password, $dbname);
-
-                                // Verifica a conexão
-                                if ($conn->connect_error) {
-                                    die("Connection failed: " . $conn->connect_error);
-                                }
-
-                                // Consulta SQL para selecionar os dados da segunda datatable
-                                $sql = "SELECT id, dia, mes, titulo, descricao, foto FROM blog";
-                                $result = $conn->query($sql);
-
-                                if ($result->num_rows > 0) {
-                                    while ($row = $result->fetch_assoc()) {
-                                        echo "<tr>";
-                                        echo "<td>" . $row["id"] . "</td>";
-                                        echo "<td><img src='../" . $row["foto"] . "' class='img-thumbnail' style='max-width:100px; max-height:100px;'></td>";
-                                        echo "<td>" . htmlspecialchars($row["dia"]) . "</td>";
-                                        echo "<td>" . htmlspecialchars($row["mes"]) . "</td>";
-                                        echo "<td>" . htmlspecialchars($row["titulo"]) . "</td>";
-                                        echo "<td>" . htmlspecialchars($row["descricao"]) . "</td>";
-                                        echo '<td class="text-right">
-                                          <button class="btn btn-link btn-warning btn-just-icon edit2" 
-                                                  data-id="' . $row["id"] . '" 
-                                                  data-dia="' . htmlspecialchars($row["dia"]) . '"
-                                                  data-mes="' . htmlspecialchars($row["mes"]) . '"
-                                                  data-titulo="' . htmlspecialchars($row["titulo"]) . '" 
-                                                  data-legenda="' . htmlspecialchars($row["descricao"]) . '"
-                                                  data-foto="' . htmlspecialchars($row["foto"]) . '">
-                                              <i class="material-icons">edit</i>
-                                          </button>
-                                        </td>';
-                                        echo "</tr>";
-                                    }
-                                } else {
-                                    echo "<tr><td colspan='7'>Nenhum resultado encontrado.</td></tr>";
-                                }
-
-                                $conn->close();
-                                ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <!-- end content-->
-                </div>
-                <!-- end card  -->
+<!-- Modal de Edição -->
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editModalLabel">Editar Evento</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <!-- end col-md-12 -->
+            <div class="modal-body">
+                <form id="editForm">
+                    <input type="hidden" id="editId" name="id">
+                    <div class="form-group">
+                        <label for="editDia">Dia <small class="text-muted"></small></label>
+                        <input type="text" class="form-control text-uppercase" id="editDia" name="dia" placeholder="DD">
+                    </div>
+                    <div class="form-group">
+                        <label for="editMes">Mês <small class="text-muted"></small></label>
+                        <input type="text" class="form-control text-uppercase" id="editMes" name="mes" placeholder="MM">
+                    </div>
+                    <div class="form-group">
+                        <label for="editTitulo">Título</label>
+                        <input type="text" class="form-control" id="editTitulo" name="titulo_1">
+                    </div>
+                    <div class="form-group">
+                        <label for="editLegenda">Legenda</label>
+                        <input type="text" class="form-control" id="editLegenda" name="legenda_1">
+                    </div>
+                    <button type="button" class="btn btn-primary" id="saveChanges">Salvar Mudanças</button>
+                </form>
+            </div>
         </div>
-        <!-- end row -->
     </div>
 </div>
 
@@ -492,7 +430,6 @@
         var mes = button.data('mes');
         var titulo = button.data('titulo');
         var legenda = button.data('legenda');
-        var foto = button.data('foto');
 
         var modal = $(this);
         modal.find('#editId').val(id);
@@ -500,24 +437,18 @@
         modal.find('#editMes').val(mes); 
         modal.find('#editTitulo').val(titulo);
         modal.find('#editLegenda').val(legenda);
-        modal.find('#editFoto').val(foto);
-        modal.find('#currentFoto').attr('src', '../' + foto);
     });
 
     $('#saveChanges').on('click', function () {
-        var form = $('#editForm')[0];
-        var formData = new FormData(form);
-
+        var form = $('#editForm');
         // Transformando a data para maiúsculas antes de enviar
         var dateInput = $('#editMes');
         dateInput.val(dateInput.val().toUpperCase());
 
         $.ajax({
             type: "POST",
-            url: "edit_event.php", // Arquivo PHP para processar a atualização
-            data: formData,
-            processData: false,
-            contentType: false,
+            url: "update_blog.php", // Arquivo PHP para processar a atualização
+            data: form.serialize(),
             success: function (response) {
                 // Mostrar um alerta de sucesso com animação
                 Swal.fire({
