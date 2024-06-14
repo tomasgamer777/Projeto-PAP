6729bb27c9d3aead10e5d1917c72ddb4a37f0512
tomasgamer777 <tomascalcada10@gmail.com>
2024-06-14 12:28:20 +0100

    Update calendar.php

--- admin/calendar.php
+++ admin/calendar.php
@@ -265,7 +265,7 @@
                   <a class="dropdown-item" href="#">Perfil</a>
                   <a class="dropdown-item" href="#">Definições</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Terminar sessão</a>
+                  <a class="dropdown-item" href="login/login.html">Terminar sessão</a>
                 </div>
               </li>
             </ul>
