9b76627edaab0896f9fe87959a02937dbccf0055
tomasgamer777 <tomascalcada10@gmail.com>
2024-06-14 08:57:44 +0100

    a

--- blog.php
+++ blog.php
@@ -101,32 +101,24 @@
         </main>
         <!-- .main-content -->
 
-        <footer class="site-footer">
-            <div class="container">
-                <img src="dummy/logo-footer.png" alt="Site Name">
-
-                <address>
-                    <p>495 Brainard St. Detroit, MI 48201 <br><a href="tel:354543543">(563) 429 234 890</a> <br> <a href="mailto:info@bandname.com">info@bandname.com</a></p>
-                </address>
-
-                <form action="#" class="newsletter-form">
-                    <input type="email" placeholder="Enter your email to join newsletter...">
-                    <input type="submit" class="button cut-corner" value="Subscribe">
-                </form>
-                <!-- .newsletter-form -->
-
-                <div class="social-links">
-                    <a href="#"><i class="fa fa-facebook-square"></i></a>
-                    <a href="#"><i class="fa fa-twitter"></i></a>
-                    <a href="#"><i class="fa fa-google-plus"></i></a>
-                    <a href="#"><i class="fa fa-pinterest"></i></a>
-                </div>
-                <!-- .social-links -->
-
-                <p class="copy">Copyright 2014 Company Name. Designed by Themezy. All right reserved</p>
-            </div>
-        </footer>
-        <!-- .site-footer -->
+        footer class="site-footer">
+				<div class="container">
+					<img src="dummy/logo-footer.png" alt="Site Name">
+					
+					<address>
+						<p>Av. Salazar 7, 3440-183 Pinheiro de Ázere <br><a href="tel:968987358">(351) 968987358</a> <br> <a href="mailto:sofilepi@gmail.com">sofilepi@gmail.com</a></p> 
+					</address> 
+					
+					<div class="social-links">
+						<a href="#"><i class="fa fa-facebook-square"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-google-plus"></i></a>
+						<a href="#"><i class="fa fa-pinterest"></i></a>
+					</div> <!-- .social-links -->
+					
+					<p class="copy">Copyright 2014 Company Name. Designed by Themezy. All right reserved</p>
+				</div>
+			</footer> <!-- .site-footer -->
 
     </div>
     <!-- #site-content -->
