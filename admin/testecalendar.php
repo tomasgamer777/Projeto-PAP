fcec1f842c93bf9805a642dfd1519d88207e6db8
tomasgamer777 <tomascalcada10@gmail.com>
2024-06-14 16:58:26 +0100

    Update testecalendar.php

--- admin/testecalendar.php
+++ admin/testecalendar.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "calendario_db";
+$username = "tomas";
+$password = "!h01fFw35";
+$dbname = "banda";
 
 // Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
