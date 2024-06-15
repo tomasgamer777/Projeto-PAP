ac8db8e6bb6fac64b82ddb81f809c01945e9d3bf
Calcadinha <89658068+tomasgamer777@users.noreply.github.com>
2024-06-15 19:55:01 +0100

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -4,6 +4,8 @@ session_start();
 // Incluir a função checkAdmin do login.php
 require_once __DIR__ . 'login/login.php'; // Ajuste o caminho conforme necessário
 checkAdmin();
+
+// Conteúdo da página de administrador
 ?>
 
 <!DOCTYPE html>
