c048988c0e94330ac5fbf06766c25bcf7e7e937c
Calcadinha <89658068+tomasgamer777@users.noreply.github.com>
2024-06-17 20:12:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -115,16 +115,9 @@
                         <ul class="slides">
                             <li>
                                 <blockquote>
-                                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo"</p>
-                                    <cite>John Smith</cite>
-                                    <span>Corporation CEO, books author</span>
-                                </blockquote>
-                            </li>
-                            <li>
-                                <blockquote>
-                                    <p>"Iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo"</p>
-                                    <cite>John Smith</cite>
-                                    <span>Corporation CEO, books author</span>
+                                    <p>&nbsp</p>
+                                    <cite>&nbsp</cite>
+                                    <span>&nbsp</span>
                                 </blockquote>
                             </li>
                         </ul>
