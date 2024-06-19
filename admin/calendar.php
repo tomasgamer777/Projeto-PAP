9be1a3af2b1cf323c5ce3b600a1b9e6c932a10b0
tomasgamer777 <tomascalcada10@gmail.com>
2024-06-19 09:32:36 +0100

    asd

--- admin/calendar.php
+++ admin/calendar.php
@@ -11,7 +11,7 @@ $user_surname = $_SESSION['user_surname'];
 $user_email = $_SESSION['user_email'];
 $user_photo = $_SESSION['user_photo'];
 
-$user_name1 = $user_name . $user_surname;
+$user_name1 = $user_name . ' ' . $user_surname;
 
 // Construir o caminho completo da foto do usuário
 $user_photo_path = '/admin/users/' . $user_photo;
