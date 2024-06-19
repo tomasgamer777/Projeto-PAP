40af1a136b4396ca3843543454bd338b75b3572a
tomasgamer777 <tomascalcada10@gmail.com>
2024-06-19 09:58:52 +0100

    d

---
+++ admin/socios/get_user_info.php
@@ -0,0 +1,28 @@
+    <?php
+    session_start();
+
+    // Verifica se o usuário está logado
+    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+        echo json_encode(["success" => false, "message" => "Usuário não está logado."]);
+        exit;
+    }
+
+    // Dados do usuário da sessão
+    $userInfo = [
+        "user_id" => $_SESSION['user_id'],
+        "nome" => $_SESSION['user_name'],
+        "sobrenome" => $_SESSION['user_surname'],
+        "email" => $_SESSION['user_email'],
+        "foto" => $_SESSION['user_photo'],
+        "telef" => $_SESSION['telef'],
+        "data_nasc" => $_SESSION['data_nasc'],
+        "morada" => $_SESSION['morada'],
+        "distrito" => $_SESSION['distrito'],
+        "cod_postal" => $_SESSION['cod_postal'],
+        "nif" => $_SESSION['nif'],
+
+        // Adicione outros campos conforme necessário
+    ];
+
+    echo json_encode(["success" => true, "data" => $userInfo]);
+    ?>
