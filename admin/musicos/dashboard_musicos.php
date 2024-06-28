9b1f45a1fb674b4b9c33d5de83b4b363e1b68628
tomasgamer777 <tomascalcada10@gmail.com>
2024-06-28 15:38:23 +0100

    Update dashboard_musicos.php

--- admin/musicos/dashboard_musicos.php
+++ admin/musicos/dashboard_musicos.php
@@ -41,8 +41,8 @@ $user_photo_path = '/admin/users/' . $user_photo;
   <div class="wrapper ">
     <div class="sidebar" data-color="rose" data-background-color="black" data-image="../assets/img/sidebar-1.jpg">
       <div class="logo">
-        <a href="dashboard.php" class="simple-text logo-mini">MM</a>
-        <a href="dashboard.php" class="simple-text logo-normal">Menu Músico</a>
+        <a href="dashboard_musicos.php" class="simple-text logo-mini">MM</a>
+        <a href="dashboard_musicos.php" class="simple-text logo-normal">Menu Músico</a>
       </div>
       <div class="sidebar-wrapper">
         <div class="user">
