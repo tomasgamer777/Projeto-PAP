6f7009acf83e1c05d06406094eefa49541b7a2b4
Calcadinha <89658068+tomasgamer777@users.noreply.github.com>
2024-07-03 23:25:45 +0100

    Update edit_user1.php

--- admin/socios/edit_user1.php
+++ admin/socios/edit_user1.php
@@ -5,8 +5,8 @@ session_start();
 require_once __DIR__ . '/../login/login.php'; // Ajuste o caminho conforme necessário
 checkAdmin();
 
-if ($_SESSION['tipo'] == 3){
-  header("Location: " . '/admin/socios/dashboard_socios.php');
+if ($_SESSION['tipo'] == 4){
+  header("Location: " . '/admin/dashboard.php');
 } else if ($_SESSION['tipo'] == 2){
   header("Location: " . '/admin/musicos/dashboard_musicos.php');
 }
